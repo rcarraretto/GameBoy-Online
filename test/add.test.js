e67d5acb694e676468e36ee8a5fcf21ba6ae24b8
Roberto Carraretto <rc.carraretto@gmail.com>
2017-11-07 19:51:32 -0200

    ADD HL, DE

--- test/add.test.js
+++ test/add.test.js
@@ -269,4 +269,75 @@ describe("add", function() {
     expect(core.FSubtract).to.equal(false);
   });
 
+
+  it("ADD HL, DE: 0 + 0", function() {
+    core.registersHL = 0x0000;
+    core.registerD = 0x00;
+    core.registerE = 0x00;
+    core.FZero = false;
+
+    core.OPCODE[0x19](core);
+
+    expect(core.registersHL).to.equal(0x0000);
+    expect(core.FHalfCarry).to.equal(false);
+    expect(core.FCarry).to.equal(false);
+    expect(core.FSubtract).to.equal(false);
+    // zero flag not affected
+    expect(core.FZero).to.equal(false);
+  });
+
+  it("ADD HL, DE: 0x1002 + 0x0A02", function() {
+    core.registersHL = 0x1002;
+    core.registerD = 0x0A;
+    core.registerE = 0x02;
+
+    core.OPCODE[0x19](core);
+
+    expect(core.registersHL).to.equal(0x1A04);
+    expect(core.FHalfCarry).to.equal(false);
+    expect(core.FCarry).to.equal(false);
+    expect(core.FSubtract).to.equal(false);
+  });
+
+  it("ADD HL, DE: 0x00FF + 0x0001 (no half carry)", function() {
+    core.registersHL = 0x00FF;
+    core.registerD = 0x00;
+    core.registerE = 0x01;
+
+    core.OPCODE[0x19](core);
+
+    expect(core.registersHL).to.equal(0x0100);
+    expect(core.FHalfCarry).to.equal(false);
+    expect(core.FCarry).to.equal(false);
+    expect(core.FSubtract).to.equal(false);
+  });
+
+  it("ADD HL, DE: 0x0FFF + 0x0001 (half carry)", function() {
+    core.registersHL = 0x0FFF;
+    core.registerD = 0x00;
+    core.registerE = 0x01;
+
+    core.OPCODE[0x19](core);
+
+    expect(core.registersHL).to.equal(0x1000);
+    // Half Carry set if carry from bit 11
+    expect(core.FHalfCarry).to.equal(true);
+    expect(core.FCarry).to.equal(false);
+    expect(core.FSubtract).to.equal(false);
+  });
+
+  it("ADD HL, DE: 0xFFFF + 0x0001 (carry)", function() {
+    core.registersHL = 0xFFFF;
+    core.registerD = 0x00;
+    core.registerE = 0x01;
+
+    core.OPCODE[0x19](core);
+
+    expect(core.registersHL).to.equal(0x0000);
+    expect(core.FHalfCarry).to.equal(true);
+    // Carry set if carry from bit 15
+    expect(core.FCarry).to.equal(true);
+    expect(core.FSubtract).to.equal(false);
+  });
+
 });
