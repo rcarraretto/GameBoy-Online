b5e575dac134b1bbd14c32c955973739b3050324
Roberto Carraretto <rc.carraretto@gmail.com>
2020-04-13 18:46:10 +0200

    enable typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,20 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
